d7ad1361a081153ff4584791cce4e0da0add6bc9
Sem34 <82157833+Sem34@users.noreply.github.com>
2022-09-05 14:51:34 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// let a = 6;
+// let b = 'Hello';
+//
+// console.log(a);
+//
+// let inputIn = document.querySelector('.input-in');
+// let button = document.querySelector('button');
+// let div = document.querySelector('.out');
+//
+// button.onclick = function (){
+//     //кнопка будет нажата
+//     console.log('Работает!!!')
+//     // console.log(inputIn.value); //value то что введено в input
+//     let b = +inputIn.value; //+ перевести в число
+//     console.log(b + 10)
+//     div.innerHTML = b;
+//     inputIn.value = '';
+//
+// }
+
+//Task 1 Создайте две переменные a = 7 и b = 9. Выведите в консоль результа умножения a на b.
+let a = 7;
+let b = 9;
+console.log(a * b);
+//Task 2 Создайте две переменные c = 7 и d = 9. Выведите на страницу результат деления c на d.
+let c = 7;
+let d = 9;
+console.log(a / b);
+// Task 3 Создайте две переменные e = 3 и f = 5. Выведите на страницу результат сложения e + f.
+let e = 3;
+let f = 5;
+console.log(e + f);
